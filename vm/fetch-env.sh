ceaa3046af219bd51fb1ca7311fd92f6321393cb
Emilien Devos <contact@emiliendevos.be>
2020-03-29 19:11:17 +0200

    fetch envs from example and allow to build locally

--- vm/fetch-env.sh
+++ vm/fetch-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+cp info.example.yml info.yml
+
 yq w -i info.yml base_args.GITLAB_OAUTH_TOKEN "$GITLAB_OAUTH_TOKEN"
 yq w -i info.yml base_args.MAVEN_AUTHORIZATION_TOKEN "$MAVEN_AUTHORIZATION_TOKEN"
 yq w -i info.yml base_args.VULTR_API_KEY "$VULTR_API_KEY"
