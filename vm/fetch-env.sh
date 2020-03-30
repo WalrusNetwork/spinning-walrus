d2226e180511d9956b247027b9641c48955772dc
Emilien Devos <contact@emiliendevos.be>
2020-03-31 00:49:12 +0200

    add netdata, status reporter and rework openrc services

--- vm/fetch-env.sh
+++ vm/fetch-env.sh
@@ -5,3 +5,4 @@ cp info.example.yml info.yml
 yq w -i info.yml base_args.GITLAB_OAUTH_TOKEN "$GITLAB_OAUTH_TOKEN"
 yq w -i info.yml base_args.MAVEN_AUTHORIZATION_TOKEN "$MAVEN_AUTHORIZATION_TOKEN"
 yq w -i info.yml base_args.VULTR_API_KEY "$VULTR_API_KEY"
+yq w -i info.yml base_args.GRAPHQL_TOKEN "$GRAPHQL_TOKEN"
