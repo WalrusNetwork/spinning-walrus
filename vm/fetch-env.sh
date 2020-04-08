d22361fcb89970021f90c0dd8f795710ef8f41b5
Emilien Devos <contact@emiliendevos.be>
2020-04-08 22:33:34 +0200

    trying new maven token

--- vm/fetch-env.sh
+++ vm/fetch-env.sh
@@ -3,6 +3,6 @@
 cp info.example.yml info.yml
 
 yq w -i info.yml base_args.GITLAB_OAUTH_TOKEN "$GITLAB_OAUTH_TOKEN"
-yq w -i info.yml base_args.MAVEN_AUTHORIZATION_TOKEN "$NEXUS_DEPLOY_KEY"
+yq w -i info.yml base_args.MAVEN_AUTHORIZATION_TOKEN "$NEXUS_NEW_DEPLOY_KEY"
 yq w -i info.yml base_args.VULTR_API_KEY "$VULTR_API_KEY"
 yq w -i info.yml base_args.GRAPHQL_TOKEN "$GRAPHQL_TOKEN"
