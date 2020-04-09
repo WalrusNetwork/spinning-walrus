4d3a4610c8a2a517bd1b8aee95f64d802aae6cef
Emilien Devos <contact@emiliendevos.be>
2020-04-09 12:57:32 +0200

    hide ngrok and python output + remove debug

--- vm/deploy.sh
+++ vm/deploy.sh
@@ -5,15 +5,14 @@ if [ ! -n "$VULTR_API_KEY" ]; then
     exit 1
 fi
 
-python3 -m http.server --directory $(pwd)/vm &
-ngrok http -log=stdout 8000 &
+python3 -m http.server --directory ${CI_PROJECT_DIR}/vm > /dev/null &
+ngrok http -log=stdout 8000 > /dev/null &
 
 sleep 10s
 
 iso_original_id=$(curl -s -H "API-Key: "${VULTR_API_KEY}"" https://api.vultr.com/v1/iso/list | jq ".[] | select(.filename==\"slim.iso\")" | jq -r ".ISOID")
-ngrok_hostname=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
-iso_download_url="${ngrok_hostname}/slim.iso"
-echo ${iso_download_url}
+ngrok_url=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+iso_download_url="${ngrok_url}/slim.iso"
 iso_new_id=$(curl -s -H "API-Key: "${VULTR_API_KEY}"" https://api.vultr.com/v1/iso/create_from_url --data "url="${iso_download_url}"" | jq -r ".ISOID")
 iso_new_status=$(curl -s -H "API-Key: "${VULTR_API_KEY}"" https://api.vultr.com/v1/iso/list | jq -r ".[] | select(.ISOID==$iso_new_id) | .status")
 
